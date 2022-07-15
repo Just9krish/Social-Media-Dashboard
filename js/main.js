0beaaa598c56aae6f579195afe5bf40345eb2353
just9krish <just9krish434@gamil.com>
2022-07-15 16:57:03 +0530

    storing and last mode in to localstorage

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
-const radioBtns = document.querySelectorAll('.toggle__wrapper input');
-const body = document.querySelector('body');
-const dark = document.getElementById('dark');
+const radioBtns = document.querySelectorAll(".toggle__wrapper input");
+const body = document.querySelector("body");
+const dark = document.getElementById("dark");
+
+const setDarkMode = () => {
+  body.classList = "dark";
+};
+
+const setLightMode = () => {
+  body.classList = "light";
+};
+
+const setColorMode = () => {
+  if (localStorage.getItem("colormode") == "dark") {
+    setDarkMode();
+    dark.click()
+  } else {
+    setLightMode();
+    light.click();
+  }
+};
 
 for (let i = 0; i < radioBtns.length; i++) {
-    radioBtns[i].addEventListener('click', e => {
-        dark.checked ? body.classList = 'dark' : body.classList = 'light'
-    })
+  radioBtns[i].addEventListener("click", (e) => {
+    if (dark.checked) {
+      localStorage.setItem("colorMode", "dark");
+      setDarkMode();
+    } else {
+      localStorage.setItem("colorMode", "dark");
+      setLightMode();
+    }
+  });
 }
+
+setColorMode();
